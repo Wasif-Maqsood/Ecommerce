a60479ce02e883fe2a37805bb28152b411a64b2b
Ali Raza <143985486+AliRaza5000@users.noreply.github.com>
2023-09-06 13:05:32 +0500

    Delete Ecommerce directory

--- Ecommerce/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from api.models import Categories, Product,Seller
-
-
-# Register your models here.
-
-admin.site.register(Categories)
-admin.site.register(Product)
-admin.site.register(Seller)
--- Ecommerce/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-
-def home_page(request):
-    print('home page requested')
-    friends= ['hamid','amish','azan']
-    return JsonResponse(friends,safe=False)
