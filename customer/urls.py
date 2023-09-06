3d5b3b95015aaffd1ce8435f790a9a71bcb2dc62
hamidamin0000@gmail.com <hamidamin0000@gmail.com>
2023-09-05 23:40:07 -0700

    add router in urls.py

--- customer/urls.py
+++ customer/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
-from customer.views import CustomerViewSet,shoping_orderViewSet,deliveriesViewSet
+from customer.views import CustomerViewSet,shoping_orderViewSet,deliveriesViewSet,categorieViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r"customers",CustomerViewSet)
 router.register(r"shoping_orders",shoping_orderViewSet)
 router.register(r"deliveries",deliveriesViewSet)
+router.register(r"categories",categorieViewSet)
 
 urlpatterns = [
     path('',include(router.urls))
